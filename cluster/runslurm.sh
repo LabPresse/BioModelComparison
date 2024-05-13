075f86470d18689f57d12df978cf39f02129dae2
Shep Bryan IV <shepperoni.pizza@gmail.com>
2024-05-13 16:20:00 -0400

    test cluster

--- cluster/runslurm.sh
+++ cluster/runslurm.sh
@@ -11,11 +11,12 @@ echo "   -$((i+1))/$num_jobs"
 printf "#!/bin/bash
 
 #SBATCH -D /home/jsbryan4/BioModelComparison/
-#SBATCH -t 0-00:05
+#SBATCH -t 0-00:15
 #SBATCH -o cluster/slurmjobs/job$i.out
 #SBATCH -e cluster/slurmjobs/job$i.err
 #SBATCH -c 1
 #SBATCH -N 1 
+#SBATCH --mem=8G
 
 module load mamba
 source activate myenv
