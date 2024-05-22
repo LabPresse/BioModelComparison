401bfa23a2de7792065547c1c12e09247b927f05
Shep Bryan IV <shepperoni.pizza@gmail.com>
2024-05-22 13:57:03 -0400

    shorter cluster run

--- cluster/runslurm.sh
+++ cluster/runslurm.sh
@@ -13,7 +13,7 @@ printf "#!/bin/bash
 #SBATCH -q public
 #SBATCH --partition=general
 #SBATCH -D /home/jsbryan4/BioModelComparison/
-#SBATCH -t 3-00:00
+#SBATCH -t 2-00:00
 #SBATCH -o cluster/slurmjobs/job$i.out
 #SBATCH -e cluster/slurmjobs/job$i.err
 #SBATCH -c 1
